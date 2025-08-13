94e6a6c458ed7a9bc6ee30a419358e503c7b0c24
Dieg0Mata <diego.a.gonzalez.mata@gmail.com>
2025-08-13 02:00:27 -0600

    el primer cambio resubido

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/AuthForm'
+
+function Page() {
+  return (
+    <div>
+        <AuthForm type="sign-in" />
+    </div>
+  )
+}
+
+export default Page
---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAKNKLHh-3BI4xXG3xncVqRyLJZc5gyUs",
+//   authDomain: "prepwise-f367d.firebaseapp.com",
+//   projectId: "prepwise-f367d",
+//   storageBucket: "prepwise-f367d.firebasestorage.app",
+//   messagingSenderId: "764813637251",
+//   appId: "1:764813637251:web:760a08f326331c0a8bcdb6",
+//   measurementId: "G-XB4G7TWBV3"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
